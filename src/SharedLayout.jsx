1450352d5d4dc623bedab76579b6893aac393d10
dolphin <fun53@ukr.net>
2023-12-23 03:30:21 +0200

    structure

---
+++ src/SharedLayout.jsx
@@ -0,0 +1,13 @@
+
+export const SharedLayout = () => {
+  return (
+		<div></div>
+    // <Wrapper>
+    //   <AppHeader />
+    //   <Suspense fallback={<div>Loading page...</div>}>
+    //     <Outlet />
+    //   </Suspense>
+    //   <Toaster />
+    // </Wrapper>
+  );
+};
