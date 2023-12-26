efa59327b8fe05c47aaa8aa96a0984c1294ef1d4
chaban-dmytro <dimach@ukr.net>
2023-12-26 10:19:33 +0100

    update

--- src/shared/api/todayApiServices.js
+++ src/shared/api/todayApiServices.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-//для перевіпки додва такий бек
 const BASE_URL = "http://watertracker.onrender.com/api";
 
 axios.defaults.baseURL = BASE_URL;
+// axios.defaults.headers.common.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ODk3NTU2YzVkNmE4NjdiMDg5NzNkYiIsImlhdCI6MTcwMzUwNzI4NiwiZXhwIjoxNzA2MDk5Mjg2fQ.k75FHO9azNcYZ6eKzQiM4XehxdA-o-GC0Gff5GGJ7kg`;
 
 export const getDayPortions = (params) => {
-  return axios.get(`/water/today/2023-12-07`);
+  console.log(params);
+  return axios.get(`/water/today/${params}`);
 };
