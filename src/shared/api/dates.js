958b825daaac64baaa7a76879a2b859f401fcb2a
dolphin <fun53@ukr.net>
2024-01-04 04:02:06 +0200

    fixadd

--- src/shared/api/dates.js
+++ src/shared/api/dates.js
@@ -1,3 +1,16 @@
-export const todayISO = (date) => {
-	return `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}` // T${date.getHours()}:${date.getMinutes()}
+const date = new Date();
+// const year = date.getFullYear();
+// const month = (date.getMonth() + 1).toString().padStart(2, "0");
+// const day = date.getDate().toString().padStart(2, "0");
+// const hours = date.getHours();
+// const minutes = date.getMinutes();
+
+export const currentDate = `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2, 0)}-${date.getDate().toString().padStart(2, 0)}`;
+// export const currentTime = `${hours}:${minutes};`;
+
+// export default currentDate;
+
+export const dateISO = (date) => {
+	return `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2, 0)}-${date.getDate().toString().padStart(2, 0)}` // T${date.getHours()}:${date.getMinutes()}
 }
+
