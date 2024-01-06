1b04c5eadae722e29c3a2e015e17fd1d1c7549b3
chaban-dmytro <dimach@ukr.net>
2024-01-06 10:17:26 +0100

    commit

--- src/components/Today/Today.jsx
+++ src/components/Today/Today.jsx
@@ -1,5 +1,5 @@
-import { useSelector, useDispatch } from "react-redux";
-import { getPortionsList } from "../../redux/water/todayOperations.js";
+import { useSelector } from "react-redux";
+// import { getPortionsList } from "../../redux/water/todayOperations.js";
 import {
   TodayWrapper,
   TodayTitle,
@@ -14,11 +14,7 @@ import {
   SvgButtonDel,
   AddWaterButton,
 } from "./Today.styled.js";
-import {
-  selectDailyPortions,
-  selectIsLoading,
-} from "../../redux/water/todaySelectors.js";
-// import IconClass from "../../images/today/Today_class.svg";
+import { selectDailyPortions } from "../../redux/water/todaySelectors.js";
 import {
   IconGlass,
   IconPencil,
@@ -26,7 +22,6 @@ import {
 } from "../../images/today/TodayIcons.jsx";
 
 export const Today = () => {
-  // const isLoading = useSelector(selectIsLoading);
   const dailyPortions = useSelector(selectDailyPortions);
 
   function onChangePortion(water, id, time) {
@@ -83,62 +78,6 @@ export const Today = () => {
       )}
     </TodayWrapper>
   );
-
-  // if (getDailyPortions() === null || getDailyPortions().length === 0) {
-  //   return <p>No result about today portions</p>;
-  // } else {
-  //   if (isLoading === true) {
-  //     return <div>Loading...</div>;
-  //   } else {
-  //     const array = getDailyPortions().map(
-  //       (portions) => portions.dailyPortions
-  //     );
-  // return (
-  //   <div className={css.wrapper}>
-  //     <h3>Today</h3>
-  //     <ul>
-  //       {array[0].map(({ water, id, time }) => {
-  //         return (
-  //           <li key={id}>
-  //             <span>{water}</span>
-  //             <span> ml </span>
-  //             <span>{time}</span>
-  //             <button type="button" onClick={() => onChangePortion(id)}>
-  //               Change
-  //             </button>
-  //             <button type="button" onClick={() => onDeletePortion(id)}>
-  //               Delete
-  //             </button>
-  //           </li>
-  //         );
-  //       })}
-  //     </ul>
-  //   </div>
-  // );
-  //   }
-  // }
-  // return (
-  //   <div className={css.wrapper}>
-  //     <h3>Today</h3>
-  //     {/* <ul>
-  //       {portions.map(({ water, id, time }) => {
-  //         return (
-  //           <li key={id}>
-  //             <span>{water}</span>
-  //             <span> ml </span>
-  //             <span>{time}</span>
-  //             <button type="button" onClick={() => onChangePortion(id)}>
-  //               Change
-  //             </button>
-  //             <button type="button" onClick={() => onDeletePortion(id)}>
-  //               Delete
-  //             </button>
-  //           </li>
-  //         );
-  //       })}
-  //     </ul> */}
-  //   </div>
-  // );
 };
 
 export default Today;
