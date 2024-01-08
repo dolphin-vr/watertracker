0469d01b78946bb2e033be791283a4a83776e766
dolphin-vr <124384373+dolphin-vr@users.noreply.github.com>
2024-01-09 00:59:34 +0200

    restore Day.jsx

--- src/components/Day/Day.jsx
+++ src/components/Day/Day.jsx
@@ -1,20 +1,26 @@
-import { DayTile, DayButton, Circle, Label } from "./Day.styled";
 import { useSelector } from "react-redux";
-import { DayTile, DayButton, Circle, Label } from "./Day.styled";
-import {
-  selectDoses,
-  selectPercentage,
-} from "../../redux/water/todaySelectors";
+import {DayTile, DayButton, Circle, Label} from "./Day.styled";
+import { selectDoses, selectPercentage } from "../../redux/water/todaySelectors";
 import { currentDate } from "../../shared/api/dates";
 import { useMemo } from "react";
 
-export const Day = ({ day, onClick }) => {
-  return (
-    <DayTile isToday={day.isToday}>
-      <DayButton onClick={() => onClick(day)}>
-        <Circle percent={day.percentage}>{day.id}</Circle>
-        <Label>{day.percentage ? `${day.percentage}  %` : ""}</Label>
-      </DayButton>
-    </DayTile>
-  );
-};
+export const Day = ({day, onClick}) => {
+  const percentage = useSelector(selectPercentage);
+  const doses = useSelector(selectDoses);
+	const newday = useMemo(()=>{
+		if (day.date===currentDate){
+		day.percentage = percentage;
+		day.doses = doses;
+	}	
+	return day
+}, [day, doses, percentage])
+
+	return (
+		<DayTile istoday={newday.isToday.toString()}>
+			<DayButton onClick={()=> onClick(newday)}>
+				<Circle percent={newday.percentage}>{newday.id}</Circle>
+				<Label>{newday.percentage ? `${newday.percentage}  %` : ""}</Label>
+			</DayButton>
+		</DayTile>
+	)
+}
